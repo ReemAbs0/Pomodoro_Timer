d10d218ec573dd1e79672819ea4539c629058896
ReemAbs0 <s12116170@stu.najah.edu>
2025-03-08 06:58:34 +0200

    changes & addition

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports={
     entry: './src/index.js',
@@ -6,4 +7,16 @@ module.exports={
         filename:'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // يستخدم هذا الملف كـ Template
+            filename: 'index.html' // سيتم إنشاء dist/index.html بناءً عليه
+        })
+    ],
+    mode: 'development'
 };
